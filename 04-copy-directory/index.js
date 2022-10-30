038059cef83f741bdae441920a4c7993a9327d34
Olga Chernega <eliang.cheng@gmail.com>
2022-10-30 13:22:14 +0200

    feat: resolve task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require("fs");
+const path = require("path");
 
+fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, err => {
+  if (err) throw err;
+});
+fs.readdir(
+  path.join(__dirname, "files"),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) throw err;
+
+    files.forEach(file => {
+      let src = path.join(__dirname, "files", file.name);
+      let dest = path.join(__dirname, "files-copy", file.name);
+      fs.copyFile(src, dest, err => {
+        if (err) throw err;
+      });
+    });
+  }
+);
