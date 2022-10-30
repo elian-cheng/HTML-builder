3c450711083f1fb91a4ea58cc039c40138943161
Olga Chernega <eliang.cheng@gmail.com>
2022-10-30 15:17:34 +0200

    refactor: task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,21 +1,45 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, err => {
-  if (err) throw err;
-});
-fs.readdir(
-  path.join(__dirname, "files"),
-  { withFileTypes: true },
-  (err, files) => {
-    if (err) throw err;
+const dest = path.join(__dirname, "files-copy");
+const src = path.join(__dirname, "files");
 
-    files.forEach(file => {
-      let src = path.join(__dirname, "files", file.name);
-      let dest = path.join(__dirname, "files-copy", file.name);
-      fs.copyFile(src, dest, err => {
-        if (err) throw err;
+function copyDir(src) {
+  fs.mkdir(dest, { recursive: true }, () => {
+    fs.readdir(src, (err, files) => {
+      if (err) throw err;
+
+      files.forEach(file => {
+        const source = path.join(src, file);
+        const destination = path.join(dest, file);
+        fs.copyFile(source, destination, () => {});
       });
     });
-  }
-);
+  });
+}
+
+fs.rm(dest, { recursive: true }, () => {
+  copyDir(src);
+});
+
+// const fs = require("fs");
+// const path = require("path");
+
+// fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, err => {
+//   if (err) throw err;
+// });
+// fs.readdir(
+//   path.join(__dirname, "files"),
+//   { withFileTypes: true },
+//   (err, files) => {
+//     if (err) throw err;
+//     files.forEach(file => {
+//       // for (let file of files) {
+//       let src = path.join(__dirname, "files", file.name);
+//       let dest = path.join(__dirname, "files-copy", file.name);
+//       fs.copyFile(src, dest, err => {
+//         if (err) throw err;
+//       });
+//     });
+//   }
+// );
